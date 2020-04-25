e73a50e004fb30707cbebf84b79900ef7ccf984f
wangjd <1209259897@qq.com>
2020-04-25 16:13:57 +0800

    添加MonthStar项目

---
+++ MonthStar/src/main/java/com/monthstar/monthstar/MonthstarApplication.java
@@ -0,0 +1,13 @@
+package com.monthstar.monthstar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonthstarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MonthstarApplication.class, args);
+    }
+
+}
---
+++ MonthStar/src/test/java/com/monthstar/monthstar/MonthstarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.monthstar.monthstar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonthstarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
